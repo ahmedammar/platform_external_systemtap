c57bfe50361ac7af76a5297b0ba23ac4fb78ad1b
Xinyu Chen <xinyu.chen@freescale.com>
2010-07-13 10:43:25 +0800

    TARGET: keep the config.* in git

---
+++ target/config.h
@@ -0,0 +1,107 @@
+/* config.h.  Generated from config.in by configure.  */
+/* config.in.  Generated from configure.ac by autoheader.  */
+
+/* Configuration/build date */
+#define DATE "2010-07-13"
+
+/* make -P prologue-searching default */
+/* #undef ENABLE_PROLOGUES */
+
+/* Define to 1 if you have the <boost/algorithm/string.hpp> header file. */
+/* #undef HAVE_BOOST_ALGORITHM_STRING_HPP */
+
+/* Define to 1 if you have the <boost/range.hpp> header file. */
+/* #undef HAVE_BOOST_RANGE_HPP */
+
+/* Define to 1 if you have the <boost/shared_ptr.hpp> header file. */
+/* #undef HAVE_BOOST_SHARED_PTR_HPP */
+
+/* Define to 1 if you have the <crash/defs.h> header file. */
+/* #undef HAVE_CRASH_DEFS_H */
+
+/* Define to 1 if you have the <elfutils/version.h> header file. */
+/* #undef HAVE_ELFUTILS_VERSION_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `dw' library (-ldw). */
+/* #undef HAVE_LIBDW */
+
+/* Define to 1 if you have the `pfm' library (-lpfm). */
+/* #undef HAVE_LIBPFM */
+
+/* have librpm */
+/* #undef HAVE_LIBRPM */
+
+/* Define to 1 if you have the `sqlite3' library (-lsqlite3). */
+/* #undef HAVE_LIBSQLITE3 */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the nss libraries. */
+/* #undef HAVE_NSS */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <tr1/memory> header file. */
+#define HAVE_TR1_MEMORY 1
+
+/* Define to 1 if you have the <tr1/unordered_map> header file. */
+#define HAVE_TR1_UNORDERED_MAP 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "systemtap"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "systemtap@sources.redhat.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "systemtap"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "systemtap 1.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "systemtap"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.2"
+
+/* Define to 1 if the C compiler supports function prototypes. */
+#define PROTOTYPES 1
+
+/* configure prefix location */
+#define STAP_PREFIX "/systemtap-1.2-macharm"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.2"
+
+/* Define like PROTOTYPES; this can be used by system headers. */
+#define __PROTOTYPES 1
